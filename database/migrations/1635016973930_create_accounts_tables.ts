a863dc84cda5a78ac4671709dc371e2d4493a1e3
Marat Zh <zmhan.box@gmail.com>
2021-10-23 22:28:38 +0300

    config: create account migration

---
+++ database/migrations/1635016973930_create_accounts_tables.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class CreateAccountsTables extends BaseSchema {
+  protected tableName = 'accounts'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('email').unique().notNullable()
+      table.string('password').notNullable()
+      table.enum('type', ['buyer', 'seller'])
+      table.double('balance').notNullable().defaultTo(0)
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
